75d2be064bb9e1131bed27366205f32913751f94
DheerajGk123 <dheerajgkrock@gmail.com>
2020-07-30 13:35:42 +0530

    demo proxy

---
+++ demo.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const port = 3000
+
+const dirPath = path.join(__dirname)
+console.log(dirPath)
+
+
+app.use(express.static(dirPath),
+
+)
+
+
+//------------------------General Scripts Basic Tage ------------------------------
+app.get('/' , function (req , res){
+
+    res.send("hello")
+
+})
+
+app.get('*' , function (req , res){
+
+    res.send("error")
+
+})
+
+console.log("started")
+
+app.listen(port ,function (){
+    console.log("server started at port :" + port)
+})
